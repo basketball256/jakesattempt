d9c14a7363e04ebaadd90f04da527fbbef49a3f1
Jake Harrison <harrison.jacob@cheverus.org>
2016-09-21 15:46:26 -0400

    rrr
    err

--- src/org/usfirst/frc5265/jakesattempt/OI.java
+++ src/org/usfirst/frc5265/jakesattempt/OI.java
@@ -17,12 +17,6 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
 import org.usfirst.frc5265.jakesattempt.subsystems.*;
 
-<<<<<<< HEAD
-// test change by BigFudge.2
-=======
-// test change by BigFudge.2.0
->>>>>>> master
-
 /**
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
